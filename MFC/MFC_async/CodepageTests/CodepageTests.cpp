6c11dc0f5f1a940e273dfc870c20b8f7ea4de9cf
Yuri Y. Kuznetsov <yyk@kestrel>
2022-08-10 18:49:19 -0400

    Updated tests

--- MFC/MFC_async/CodepageTests/CodepageTests.cpp
+++ MFC/MFC_async/CodepageTests/CodepageTests.cpp
@@ -15,6 +15,32 @@ public:
 TEST(CodepageF, t1)
 {
     std::cout << "sizeof(CHAR): " << sizeof(CHAR) << std::endl;
+    std::cout << "sizeof(WCHAR): " << sizeof(WCHAR) << std::endl;
 
     std::cout << "Hello" << std::endl;
+    std::wcout << L"Hello" << std::endl;
+}
+
+TEST(CodepageF, t2)
+{
+    std::wstring s(L"Пассатижи");
+    for (auto c : s)
+        printf("\\x%02x", c & 255);
+    printf("\n");
+
+    std::wcout << "sizeof(s[0]):" << sizeof(s[0]) << std::endl;
+    std::wcout << "s.size():" << s.size() << std::endl;
+    std::wcout << "s:" << s << std::endl;
+}
+
+TEST(CodepageF, t3)
+{
+    std::string s("Пассатижи");
+    for (auto c : s)
+        printf("\\x%02x", c & 255);
+    printf("\n");
+
+    std::cout << "sizeof(s[0]):" << sizeof(s[0]) << std::endl;
+    std::cout << "s.size():" << s.size() << std::endl;
+    std::cout << "s:" << s << std::endl;
 }
