231bcd7738dd8e5774761201f49f3fff7d227b3b
Yuri Y. Kuznetsov <yyk@kestrel>
2022-08-10 20:47:08 -0400

    Updated tests

--- MFC/MFC_async/CodepageTests/CodepageTests.cpp
+++ MFC/MFC_async/CodepageTests/CodepageTests.cpp
@@ -2,8 +2,11 @@
 //
 //
 
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <iostream>
 #include <wtypes.h>
+#include <stringapiset.h>
 #include <gtest/gtest.h>
 
 class CodepageF : public testing::Test
@@ -43,4 +46,56 @@ TEST(CodepageF, t3)
     std::cout << "sizeof(s[0]):" << sizeof(s[0]) << std::endl;
     std::cout << "s.size():" << s.size() << std::endl;
     std::cout << "s:" << s << std::endl;
+
+    {
+        int wchars_num = MultiByteToWideChar(CP_UTF8, 0, s.c_str(), -1, NULL, 0);
+        std::vector<wchar_t> wstr(wchars_num);
+
+        MultiByteToWideChar(CP_UTF8, 0, s.c_str(), -1, wstr.data(), int(wstr.size()));
+        
+        for (auto c : wstr)
+            printf("\\x%04x", c & 0xffff);
+        printf("\n");
+
+        FILE* fp = fopen("CodepageF_t3.txt", "wb");
+
+        ASSERT_TRUE(fp != NULL);
+        fwrite("\xff\xfe", 2, 1, fp);
+        fwrite(wstr.data(), wstr.size(), sizeof(wstr[0]), fp);
+        fclose(fp);
+    }
+}
+
+TEST(CodepageF, t4)
+{
+    {
+        uint8_t s[] = u8"1234567";
+        printf("sizeof(s) = %d\n", (int)sizeof(s));
+        printf("strlen(s) = %d\n", (int)strlen((const char*)s));
+        for (auto c : s)
+            printf("\\x%02x", c & 255);
+        printf("\n");
+    }
+    {
+        char s[] = u8"Пассатижи";
+        printf("sizeof(s) = %d\n", (int)sizeof(s));
+        printf("strlen(s) = %d\n", (int)strlen((const char*)s));
+        for (auto c : s)
+            printf("\\x%02x", c & 255);
+        printf("\n");
+        FILE* f = fopen("t4.txt", "wb");
+        ASSERT_NE(nullptr, f);
+        fputs((const char*)s, f);
+        fclose(f);
+    }
+    {
+        uint8_t s[] = "Пассатижи";
+        printf("strlen(s) = %d\n", (int)strlen((const char*)s));
+        for (auto c : s)
+            printf("\\x%02x", c & 255);
+        printf("\n");
+    }
+    //std::wcout << "sizeof(s[0]):" << sizeof(s[0]) << std::endl;
+    //std::wcout << "s.size():" << s.size() << std::endl;
+    //std::wcout << "s:" << s << std::endl;
 }
