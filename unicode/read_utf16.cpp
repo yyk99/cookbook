82bf56e1afb309bcc819a8f0bc2bac7e28c39c96
Yuri Y. Kuznetsov <yyk@kestrel>
2022-11-19 18:08:50 -0500

    Added more unicode tests files

--- unicode/read_utf16.cpp
+++ unicode/read_utf16.cpp
@@ -10,28 +10,42 @@
 
 int main(int argc, char** argv)
 {
-    if (argc == 2)
+    if (argc >= 2)
     {
-        // open as a byte stream
-        std::wifstream fin(argv[1], std::ios::binary);
-        if (fin) {
-            // apply BOM-sensitive UTF-16 facet
-            fin.imbue(std::locale(fin.getloc(), new std::codecvt_utf16<wchar_t, 0x10ffff, std::consume_header>));
-            std::cout << "Current locale : " << fin.getloc().name() << '\n';
-            // read     
-            for (wchar_t c; fin.get(c); )
-            {
-                // std::cout << std::showbase << std::hex << c << '\n';
-                if (char c8 = (c >> 8) & 255) {
-                    std::cout << "\\U+"
-                        << std::hex << std::setw(4) << std::setfill('0')
-                        << (unsigned)c;
-                } else {
-                    std::cout << char(c&255);
+        for (int i = 1; i < argc; ++i)
+        {
+            std::cout << "File: " << argv[i] << "\n";
+            std::wifstream fin(argv[i], std::ios::binary);
+            if (fin) {
+                // apply BOM-sensitive UTF-16 facet
+#if 1
+                fin.imbue(std::locale(fin.getloc(), new std::codecvt_utf16<wchar_t, 0x10ffff, std::consume_header>));
+#else
+                fin.imbue(std::locale(fin.getloc(), new std::codecvt_utf16<wchar_t>));
+#endif
+                std::cout << "Current locale : " << fin.getloc().name() << '\n';
+                // read
+                for (wchar_t c; fin.get(c); )
+                {
+                    std::cout
+                        << "0x"
+                        << std::setw(4)
+                        << std::setfill('0')
+                        << std::hex
+                        << c << '\n';
+#if 0
+                    if (char c8 = (c >> 8) & 255) {
+                        std::cout << "\\U+"
+                            << std::hex << std::setw(4) << std::setfill('0')
+                            << (unsigned)c;
+                    } else {
+                        std::cout << char(c & 255);
+                    }
+#endif
                 }
+            } else {
+                std::cerr << "Cannot open: " << argv[i] << " file\n";
             }
-        } else {
-            std::cerr << "Cannot open: " << argv[1] << " file\n";
         }
     } else {
         std::cerr << "Usage: " << argv[0] << " file\n";
